94753dd05ef10e99aacbd8f62598ae187122ae33
Garion Herman <garion@silverstripe.com>
2016-01-16 20:03:06 +1300

    Implemented basic working version.

---
+++ code/extensions/ControllerExtension.php
@@ -0,0 +1,46 @@
+<?php
+namespace Cheddam\SilverStripeFlashMessages;
+
+use \ArrayList, \ArrayData;
+use \Extension;
+use \Session;
+
+class ControllerExtension extends Extension
+{
+	/**
+	 * Stores a flash message to be displayed on the next page.
+	 * @param  string $message The message to display
+	 * @param  string $type    (Optional) A class (or classes) to apply to the message.
+	 */
+	public function flashMessage($message, $type = '')
+	{
+		// Get the existing flash message array (if it exists)
+		$messages = Session::get('flashmessages') ? json_decode(Session::get('flashmessages')) : [];
+
+		// Append the new message to the array
+		$messages[] = ['Message' => $message, 'Type' => $type];
+
+		// Update the flash message array
+		Session::set('flashmessages', json_encode($messages));
+	}
+
+	public function FlashMessages()
+	{
+		$messages = Session::get('flashmessages') ? json_decode(Session::get('flashmessages'), true) : [];
+
+		// todo: make silverstripe not stupid
+		$return = new ArrayList();
+		foreach ($messages as $message) {
+			$return->push(new ArrayData($message));
+		}
+
+		Session::clear('flashmessages');
+
+		return $return;
+	}
+
+	public function FlashMessagesPresent()
+	{
+		return Session::get('flashmessages') ? true : false;
+	}
+}
---
+++ readme.md
@@ -0,0 +1,25 @@
+# silverstripe-flash-messages
+
+for flashing dem messages
+
+## Usage
+
+`composer install rentaswag/silverstripe-flash-messages`
+
+In controller methods:
+
+`$this->flashMessage('She drove me here', 'warning');`
+
+In templates:
+
+```php
+<% if $FlashMessagesPresent %>
+	<% loop $FlashMessages %>
+		<div class="message $Type">$Message</div>
+	<% end_loop %>
+<% end_if %>
+```
+
+## Caveats
+
+- Currently $FlashMessages can only be called once as it cleans up the messages before returning them. Currently looking for a better way to handle this.
